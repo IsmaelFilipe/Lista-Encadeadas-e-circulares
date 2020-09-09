3bd764971d7ed20e7f52a6db8f1b7a235a932b51
Ismael Filipe <ismaelfilipe59@hotmail.com>
2020-09-09 12:06:16 -0300

    !!!

---
+++ Exercicio02/Circular.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio02
+{
+    public class Circular
+    {
+
+        public int soldado;
+        public LinkedList<string> soldados;
+        public int x, numero, count,i;
+
+        public void Circ()
+        {
+            soldados = new LinkedList<string>();
+            soldados.AddLast("A");
+            soldados.AddLast("B");
+            soldados.AddLast("C");
+            soldados.AddLast("D");
+            soldados.AddLast("E");
+            soldados.AddLast("F");
+            Random numAleatorio = new Random();
+            numero = numAleatorio.Next(1, 11);
+            int aux = soldados.Count;
+            soldado = numAleatorio.Next(0, aux);
+
+            Console.WriteLine($"Numero sorteado: {numero}");
+
+            foreach (var item in soldados)
+            {
+                if (x == soldado)
+                {
+                    Console.WriteLine($"Soldado {item}");
+                }
+                x++;
+            }        
+            while (soldados.Count > 1)
+            {
+                soldados.Remove(remove());
+                count = 0;
+            }
+            foreach(var item in soldados)
+                Console.WriteLine($"\nSoldado remanecente: {item}");
+            Console.ReadLine();
+        }
+
+        public string remove()
+        {           
+            string aux = "";
+            while (count < numero+1)
+            {
+                foreach (var item in soldados)
+                {
+                    if (i == soldado)
+                    {
+                        if (count == numero)
+                        {
+                            aux = item;
+                        }
+                        count++;
+                      
+                    }
+                    else
+                        i++;
+                }
+            }
+            return aux;
+        }
+    }
+}
--- Exercicio02/Program.cs
+++ Exercicio02/Program.cs
@@ -1,64 +1,19 @@
 ﻿using Microsoft.VisualBasic.CompilerServices;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace Exercicio02
 {
     class Program
-    {
+    {        
         static void Main(string[] args)
-        {
-            LinkedList<string> soldados = new LinkedList<string>();
-            soldados.AddLast("A");
-            soldados.AddLast("B");
-            soldados.AddLast("C");
-            soldados.AddLast("D");
-            soldados.AddLast("E");
-            soldados.AddLast("F");
+        {           
 
-            Random numAleatorio = new Random();
-            int numero = numAleatorio.Next(1, 16);
-            int aux = soldados.Count;
-            int soldado = numAleatorio.Next(0, aux);
+            Circular circular = new Circular();
 
-            Console.WriteLine($"Numero sorteado: {numero}");
-            int x = 0;
-            foreach(var item in soldados)
-            {
-                if(x == soldado)
-                {
-                    Console.WriteLine($"Soldado {item}");
-                }
-                x++;
-            }
-            int count = 0;
-            bool pos = true;
-
-            while (soldados.Count > 1)
-            {
-                x = 0;
-                foreach (var item in soldados)
-                {
-                    if(pos == true)
-                    {
-                        if (x == soldado)
-                        {
-                            Console.WriteLine($"Soldado {item}");
-                            pos = false;
-                        }
-                        x++;
-                    }
-                    else
-                    {
-                        if(count == numero)
-                        {
-                            soldados.Remove(item);
-                        }
-                        count++;
-                    }                    
-                    
-                }
-            }
+            circular.Circ();
         }
+        
     }
 }
